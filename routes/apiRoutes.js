5b3e51933af7daa118db0ad6414dbf94d0fda709
Girverius <gsmac1224@gmail.com>
2021-02-10 23:45:32 -0600

    corrected models and server

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+
+//create workout
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+// router.post("/api/", ({body}, res) => {
+//     Workout.create(body)
+//     .then(dbWorkout => {
+//         res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//         res.status(400).json(err);
+//     });
+// })
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const { SSL_OP_TLS_BLOCK_PADDING_BUG } = require("constants");
 const express = require("express");
-const express = require("express");
+const logger = require("morgan")
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
 });
 
 //routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
