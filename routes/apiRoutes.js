abb2b4e641dcadb47b72e255fad653dd15704758
Girverius <gsmac1224@gmail.com>
2021-02-12 22:48:59 -0600

    added route put and get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,14 +13,24 @@ router.post("/api/workouts", ({body}, res) => {
     });
 });
 
-// router.post("/api/", ({body}, res) => {
-//     Workout.create(body)
-//     .then(dbWorkout => {
-//         res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//         res.status(400).json(err);
-//     });
-// })
+router.put("/api/workouts/:id", ({body, params}, res) => {
+    Workout.findByIdAndUpdate({_id: params.id}, { $push: {exercises: body} }, { new: true })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err =>{
+        res.status(400).json(err);
+    });
+});
 
-module.exports = Workout;
+    router.get("/api/workouts", ({body}, res) => {
+        Workout.find(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
